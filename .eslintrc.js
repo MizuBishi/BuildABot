ec1832d82dc84e3be2bbefb90a095d34b4aa05b4
Michelle Andrey <michelle.andrey@kilokilo.ch>
2020-10-28 11:35:13 +0100

    Add chooseable parts for robot.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['plugin:vue/essential', '@vue/airbnb', 'eslint:recommended'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
